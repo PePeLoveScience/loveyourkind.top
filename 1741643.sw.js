1c63ce56bf8949ea952bd569f3a12194b0c8a34f
PePeLoveScience <86812400+PePeLoveScience@users.noreply.github.com>
2025-08-02 12:50:05 +0700

    123

---
+++ 1741643.sw.js
@@ -0,0 +1,5 @@
+self.opts = {
+    zoneID: 1741643,
+    swDomain: "sdk4push.com",
+}
+importScripts("https://sdk4push.com/f/sw.js")
---
+++ 1741644.sw.js
@@ -0,0 +1,5 @@
+self.opts = {
+    zoneID: 1741644,
+    swDomain: "sdk4push.com",
+}
+importScripts("https://sdk4push.com/f/sw.js")
